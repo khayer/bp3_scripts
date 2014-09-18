80877b39e0953909c2571102b05bbc78417f2132
Katharina Hayer <katharinaehayer@gmail.com>
2014-09-18 15:01:29 -0400

    backup

--- find_differential_expressed_genes_gene_level.rb
+++ find_differential_expressed_genes_gene_level.rb
@@ -1,6 +1,45 @@
+require "csv"
 spliceforms = ARGV[0]
 list_of_diff_genes = ARGV[1]
 refseq_to_symbol = ARGV[2]
+counts = ARGV[3] # data.txt
+
+def sample_variance(num_array)
+  m = mean(num_array)
+  sum = num_array.inject(0){|accum, i| accum + (i - m) ** 2 }
+  return sum / (num_array.length - 1).to_f
+end
+
+def sd(num_array)
+  return Math.sqrt(sample_variance(num_array))
+end
+
+def mean(num_array)
+  num_array.instance_eval { reduce(:+) / size.to_f }
+end
+
+def coefficient_of_variation(num_array)
+  c = sd(num_array)/mean(num_array)
+end
+
+cov = {}
+CSV.foreach(counts,:col_sep => "\t") do |ps|
+  next unless ps[0] =~ /^gene/
+  num_array = ps[1..4].map { |e|  e.to_f + 1.0 }
+  mean_g1 = mean(num_array)
+  sd_g1 = sd(num_array)
+  cov_g1 = coefficient_of_variation(num_array)
+  #cov_g1 = 0 if cov_g1.nan?
+  num_array = ps[5..8].map { |e|  e.to_f + 1.0 }
+  mean_g2 = mean(num_array)
+  sd_g2 = sd(num_array)
+  cov_g2 = coefficient_of_variation(num_array)
+  #cov_g2 = 0 if cov_g2.nan?
+  cov[ps[0].split(":")[1]] = [cov_g1,mean_g1,sd_g1,cov_g2,mean_g2,sd_g2]
+end
+
+puts cov
+exit
 
 names = {}
 File.open(refseq_to_symbol).each { |line| names[line.chomp.split("\t")[1]] = line.chomp.split("\t")[0] }
@@ -16,6 +55,7 @@ header = %w{feature_type feature_id  chromosome  start end mean_celecoxib_il1b m
 genes = []
 out = []
 
+puts "ensembl\tsymbol\tfold_change\tmean_g1\tmean_g2\tcov_g1\tcov_g2"
 File.open(list_of_diff_genes).each do |line|
   line.chomp!
   line = line.split("\t")
@@ -25,7 +65,7 @@ File.open(list_of_diff_genes).each do |line|
   #STDERR.puts line[header.index("symbol")]
   if spliceforms_num[names[line[header.index("symbol")]]]  == "1"
 
-    puts "#{line[header.index("symbol")]}\t#{line[header.index("fold")]}\t#{line[5]}\t#{line[6]}" unless genes.include?(line[header.index("symbol")])
-    genes << line[header.index("symbol")] unless genes.include?(line[header.index("symbol")])
+    puts "#{line[header.index("feature_id")]}\t#{line[header.index("symbol")]}\t#{line[header.index("fold")]}\t#{line[5]}\t#{line[6]}\t#{cov[line[header.index("feature_id")]][0]}\t#{cov[line[header.index("feature_id")]][1]}" unless genes.include?(line[header.index("feature_id")])
+    genes << line[header.index("feature_id")] unless genes.include?(line[header.index("feature_id")])
   end
 end
--- merge.rb
+++ merge.rb
@@ -1,28 +1,44 @@
+require "csv"
 file1 = ARGV[0]
 file2 = ARGV[1]
 file3 = ARGV[2]
 
+
+header = %w{ensembl symbol fold_change mean_g1 mean_g2 cov_g1 cov_g2}
 counts_1 = {}
 
-File.open(file1).each do |line|
-  line.chomp!
-  fields = line.split("\t")
-  counts_1[fields[0]] = fields[1].to_f
+#File.open(file1).each do |line|
+CSV.foreach(file1,:col_sep => "\t",:headers => true) do |ps|
+  counts_1[ps["ensembl"]] = [ps["fold_change"].to_f,ps["mean_g1"].to_f,ps["mean_g2"].to_f,
+    ps["cov_g1"].to_f,ps["cov_g2"].to_f]
 end
 
 counts_2 = {}
 
-File.open(file2).each do |line|
-  line.chomp!
-  fields = line.split("\t")
-  counts_2[fields[0]] = fields[1].to_f
+#File.open(file2).each do |line|
+CSV.foreach(file2,:col_sep => "\t",:headers => true) do |ps|
+  #next unless line =~ /ENS/
+  #line.chomp!
+  #fields = line.split("\t")
+  #counts_2[fields[header.index("ensembl")]] = fields[header.index("fold_change")].to_f
+  counts_2[ps["ensembl"]] = [ps["fold_change"].to_f,ps["mean_g1"].to_f,ps["mean_g2"].to_f,
+    ps["cov_g1"].to_f,ps["cov_g2"].to_f]
 end
 
-File.open(file3).each do |line|
-  line.chomp!
-  fields = line.split("\t")
-  if counts_1[fields[0]] && counts_2[fields[0]]
-    average = (fields[1].to_f+ counts_1[fields[0]] + counts_2[fields[0]]) / 3.0
-    puts "#{fields[0]}\t#{average}"
+#puts counts_2
+#exit
+
+puts "ensembl\tsymbol\tfold_change_average\tcoe_of_var_aver\tfold_change_cele\tfold_change_vehicle\tfold_change_rofe\tcoe_of_var_g1_cele\tcoe_of_var_g2_cele\tcoe_of_var_g1_vehicle\tcoe_of_var_g2_vehicle\tcoe_of_var_g1_rofe\tcoe_of_var_g2_rofe"
+CSV.foreach(file3,:col_sep => "\t",:headers => true) do |ps|
+#File.open(file3).each do |line|
+  if counts_1[ps["ensembl"]] && counts_2[ps["ensembl"]]
+    average = (ps["fold_change"].to_f+ counts_1[ps["ensembl"]][0] +
+      counts_2[ps["ensembl"]][0]) / 3.0
+    average_cov = (ps["cov_g1"].to_f+ ps["cov_g2"].to_f+counts_1[ps["ensembl"]][3] +
+      counts_1[ps["ensembl"]][4] +counts_2[ps["ensembl"]][3] +counts_2[ps["ensembl"]][4] ) / 6.0
+    l = "#{ps["ensembl"]}\t#{ps["symbol"]}\t#{average}\t#{average_cov}\t#{counts_1[ps["ensembl"]][0]}\t#{counts_2[ps["ensembl"]][0]}\t#{ps["fold_change"].to_f}"
+    l += "\t#{counts_1[ps["ensembl"]][3]}\t#{counts_1[ps["ensembl"]][4]}\t#{counts_2[ps["ensembl"]][3]}\t#{counts_2[ps["ensembl"]][4]}"
+    l += "\t#{ps["cov_g1"]}\t#{ps["cov_g2"]}"
+    puts l
   end
 end
